606f5a3899bc663a74a1bc7e177fe5f06f497544
Amare <leaved@abv.bg>
2021-11-22 15:13:50 +0200

    add BLL Clonedata and Convertor using reflection

---
+++ HotVagoBLL/BLL/CloneData/CloneData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotVagoBLL.BLL.CloneData
+{
+    public class CloneData<TSource, TDestination>
+    {
+        public static TDestination CopyProperties(TSource source)
+        {
+            var target = (TDestination)Activator.CreateInstance(typeof(TDestination));
+
+            Type objTypeBase = source.GetType();
+            Type objTypeTarget = target.GetType();
+
+            PropertyInfo _propinfo = null;
+            var propInfos = objTypeBase.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+            foreach (var propInfo in propInfos)
+            {
+                try
+                {
+                    _propinfo = objTypeTarget.GetProperty(propInfo.Name, BindingFlags.Instance | BindingFlags.Public);
+                    if (_propinfo != null)
+                    {
+                        _propinfo.SetValue(target, propInfo.GetValue(source));
+                    }
+                }
+                catch (ArgumentException aex) { if (!string.IsNullOrEmpty(aex.Message)) continue; }
+                catch (Exception ex) { if (!string.IsNullOrEmpty(ex.Message)) return default(TDestination); }
+            }
+
+            return target;
+        }
+    }
+}
---
+++ HotVagoBLL/BLL/CloneData/Convertor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotVagoBLL.BLL.CloneData
+{
+   public class Convertor<TSource, TDestination> : CloneData<TSource, TDestination>
+        where TSource : HotVagoDAL.Models.Common
+        where TDestination : IDto
+    {
+    }
+}
